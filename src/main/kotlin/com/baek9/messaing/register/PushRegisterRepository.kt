0f10d0c347dcf2528e57bd49c92cc43725e0fe2c
Dltmd202 <dltmd202@gmail.com>
2024-05-14 11:45:18 +0900

    feat: push notification record produce

--- src/main/kotlin/com/baek9/messaing/register/PushRegisterRepository.kt
+++ src/main/kotlin/com/baek9/messaing/register/PushRegisterRepository.kt
@@ -2,5 +2,11 @@ package com.baek9.messaing.register
 
 import com.baek9.messaing.register.entity.ReservedPushRegister
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
 
-interface PushRegisterRepository : JpaRepository<ReservedPushRegister, Long>
+interface PushRegisterRepository : JpaRepository<ReservedPushRegister, Long> {
+
+    @Query("select r from ReservedPushRegister r where r.atTime >= :current and r.atTime < :end and r.commited = false")
+    fun getNotCommitedPeriodReservation(current: LocalDateTime, end: LocalDateTime): List<ReservedPushRegister>
+}
